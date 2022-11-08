e744c3505c3e4458810185fd4e2ef4e524df021d
crazycat9 <crazycat9@163.com>
2022-11-08 22:34:06 +0800

    save nvim

--- nvim/lua/conf/cmp.lua
+++ nvim/lua/conf/cmp.lua
@@ -54,14 +54,9 @@ cmp_window.info = function(self)
 	return info
 end
 
--- local check_backspace = function()
--- 	local col = vim.fn.col "." - 1
--- 	return col == 0 or vim.fn.getline("."):sub(col, col):match "%s"
--- end
-
-local has_words_before = function()
-	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+local check_backspace = function()
+	local col = vim.fn.col "." - 1
+	return col == 0 or vim.fn.getline("."):sub(col, col):match "%s"
 end
 
 local options = {
@@ -97,50 +92,23 @@ local options = {
 		["<C-f>"] = cmp.mapping.scroll_docs(4),
 		["<C-Space>"] = cmp.mapping.complete(),
 		["<C-e>"] = cmp.mapping.close(),
-		["<CR>"] = cmp.mapping.confirm {
-			behavior = cmp.ConfirmBehavior.Insert,
-			select = true,
-		},
-		-- ["<Tab>"] = cmp.mapping(function(fallback)
-		-- 	if cmp.visible() then
-		-- 		cmp.select_next_item()
-		-- 	elseif luasnip.expandable() then
-		-- 		luasnip.expand()
-		-- 	elseif luasnip.expand_or_jumpable() then
-		-- 		luasnip.expand_or_jump()
-		-- 	elseif check_backspace() then
-		-- 		fallback()
-		-- 	else
-		-- 		fallback()
-		-- 	end
-		-- end, {
-		-- 	"i",
-		-- 	"s",
-		-- }),
-		-- ["<S-Tab>"] = cmp.mapping(function(fallback)
-		-- 	if cmp.visible() then
-		-- 		cmp.select_prev_item()
-		-- 	elseif luasnip.jumpable(-1) then
-		-- 		luasnip.jump(-1)
-		-- 	else
-		-- 		fallback()
-		-- 	end
-		-- end, {
-		-- 	"i",
-		-- 	"s",
-		-- }),
+		["<CR>"] = cmp.mapping.confirm { select = true },
 		["<Tab>"] = cmp.mapping(function(fallback)
 			if cmp.visible() then
 				cmp.select_next_item()
+			elseif luasnip.expandable() then
+				luasnip.expand()
 			elseif luasnip.expand_or_jumpable() then
 				luasnip.expand_or_jump()
-			elseif has_words_before() then
-				cmp.complete()
+			elseif check_backspace() then
+				fallback()
 			else
 				fallback()
 			end
-		end, { "i", "s" }),
-
+		end, {
+			"i",
+			"s",
+		}),
 		["<S-Tab>"] = cmp.mapping(function(fallback)
 			if cmp.visible() then
 				cmp.select_prev_item()
@@ -149,7 +117,10 @@ local options = {
 			else
 				fallback()
 			end
-		end, { "i", "s" }),
+		end, {
+			"i",
+			"s",
+		}),
 	},
 	sources = {
 		{ name = "nvim_lsp" },
