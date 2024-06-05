d0567c336cfadabab3c06594fc991931b8129963
kk <kk>
2024-06-05 11:04:43 +0800

    1

--- nvchad/lua/chadrc.lua
+++ nvchad/lua/chadrc.lua
@@ -5,14 +5,15 @@
 local M = {}
 
 M.ui = {
-    theme = "chadracula",
+  theme = "chadracula",
+  transparency = true,
 
-    telescope = { style = "bordered" }, -- borderless / bordered
+  telescope = { style = "bordered" }, -- borderless / bordered
 
-    hl_override = {
-        Comment = { italic = true },
-        ["@comment"] = { italic = true },
-    },
+  hl_override = {
+    Comment = { italic = true },
+    ["@comment"] = { italic = true },
+  },
 }
 
 return M
