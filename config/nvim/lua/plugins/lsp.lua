085aa18dc1347a4573e7533079e2f309921784da
Crazycat <41771911+j2501987804@users.noreply.github.com>
2025-12-04 17:02:17 +0800

    Create lsp.lua

---
+++ config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,65 @@
+return {
+    {
+        'neovim/nvim-lspconfig',
+        dependencies = {
+            'williamboman/mason.nvim',
+        },
+        config = function()
+            -- lsp
+            require("mason").setup()
+            local mr = require("mason-registry")
+            mr.refresh(function()
+                -- stylua: ignore
+                local ensure_installed = {
+                    -- lua
+                    "lua-language-server", "stylua",
+                    -- sh
+                    "bash-language-server", "shfmt",
+                    -- go
+                    "delve", "gopls", "gomodifytags", "impl", "goimports", "gofumpt",
+                }
+
+                for _, tool in ipairs(ensure_installed) do
+                    local p = mr.get_package(tool)
+                    if not p:is_installed() then
+                        p:install()
+                    end
+                end
+            end)
+            vim.lsp.enable({ 'gopls', 'lua_ls', "bashls" })
+        end,
+    },
+
+    {
+        'stevearc/conform.nvim',
+        opts = {
+            notify_on_error = false,
+            format_on_save = function(bufnr)
+                return {
+                    timeout_ms = 500,
+                    lsp_format = 'fallback',
+                }
+            end,
+            formatters_by_ft = {
+                go = { "goimports", "gofumpt" },
+            },
+
+        }
+    },
+
+    {
+        "nvimtools/none-ls.nvim",
+        config = function()
+            local nls = require("null-ls")
+            nls.setup({
+                sources = {
+                    nls.builtins.code_actions.gomodifytags,
+                    nls.builtins.code_actions.impl,
+                    nls.builtins.formatting.goimports,
+                    nls.builtins.formatting.gofumpt,
+                },
+            })
+        end
+    },
+    { "j-hui/fidget.nvim", opts = {} }
+}
